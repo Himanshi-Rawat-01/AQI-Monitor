6a05f396032f9fbff2b085dfa7a7ad81f89e15d3
Test User <test@example.com>
2026-04-11 21:07:43 +0530

    feat: added interactive Galaxy WebGL background and removed dark overlays

--- pollution-app/react-app/src/App.jsx
+++ pollution-app/react-app/src/App.jsx
@@ -2,14 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Galaxy from './components/Galaxy'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -5, pointerEvents: 'none' }}>
+        <Galaxy />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   )
 }
 
